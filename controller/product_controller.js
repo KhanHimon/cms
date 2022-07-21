a4d6b4af69b0e5b238f26b59030ccf19545a494e
Luacs Khan <ltkhang98@gmail.com>
2022-07-21 16:10:18 +0700

    update theme

---
+++ controller/product_controller.js
@@ -0,0 +1,5 @@
+class Product {
+    hien_thi_tat_du_an(req,res) {
+        res.render('pages/du_an');
+    }
+}
--- routes/index.route.js
+++ routes/index.route.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const login_controller = require('../controller/login_controller');
+const product_controller = require('../controller/product_controller');
 /* GET home page. */
 router.get('/',login_controller.loginRequired, function(req, res, next) {
-  res.render('index', { title: 'Express'});
+  res.render('index');
+});
+router.get('/du-an',login_controller.loginRequired, function(req, res, next) {
+  res.render('pages/du_an');
 });
 router.post('/logout', login_controller.logout);
 
