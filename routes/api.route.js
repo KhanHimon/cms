f4094bf2e54e1baa7fb3fe2512e7757c6b51308c
Luacs Khan <ltkhang98@gmail.com>
2022-12-06 08:33:17 +0700

    update

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const cors = require('cors');
 const API_CONTROLLER = require('../controller/api_controller');
 
 router.get('/users' ,API_CONTROLLER.API_USER)
